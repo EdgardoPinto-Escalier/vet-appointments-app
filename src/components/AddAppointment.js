fe211625fb34d71c55dc50ee2a6d00a5ce51cc00
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2018-09-05 12:32:13 +0200

    Created Header component

---
+++ src/components/AddAppointment.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class AddAppointment extends Component {
+
+  render() {
+    return (
+      <p>From Add Appointment</p>
+    );
+  }
+}
+
+export default AddAppointment;
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import Header from './Header';
+import AddAppointment from './AddAppointment';
 
 
 
 class App extends Component {
   render() {
     return (
+      <div className="container">
+        <Header
+          title="VET APPOINTMENTS APP"
+        />
 
+        <div className="row">
+          <div className="col-md-6">
+            <AddAppointment 
+            
+            />
+          </div>
+          <div className="col-md-6">
+            2
+          </div>
+        </div>
+      </div>
     );
   }
 }
