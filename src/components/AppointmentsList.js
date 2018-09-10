5e8eaad7ae2301a59175b6902dab31e3b83ecce6
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2018-09-10 20:32:04 +0200

    New commit

--- src/components/AppointmentsList.js
+++ src/components/AppointmentsList.js
@@ -4,8 +4,12 @@ class AppointmentsList extends Component {
   
   render() {
     return ( 
+        <div className="card mt-5">
+            <div className="card-body">
+                <h2 className="card-title text-center">Manage Appointments Here</h2>
+            </div>
+        </div>
 
-      
      )
   }
 }
