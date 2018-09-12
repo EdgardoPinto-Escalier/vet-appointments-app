7f1021ed169d009bb7db47846cb3cf257c9217b3
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2018-09-12 22:47:17 +0200

    Created new component Appointment.js

--- src/components/AppointmentsList.js
+++ src/components/AppointmentsList.js
@@ -7,6 +7,11 @@ class AppointmentsList extends Component {
         <div className="card mt-5">
             <div className="card-body">
                 <h3 className="card-title text-center">Manage Appointments Here</h3>
+                <div className="lista-citas">
+                    {Object.keys(this.props.appointments).map(appointment => (
+                        
+                    ))}
+                </div>
             </div>
         </div>
 
