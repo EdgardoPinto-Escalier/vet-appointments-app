2e4380808a0596b4c467d18dc97a7379444e9b25
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2018-09-13 17:13:38 +0200

    New commit

--- src/components/Appointment.js
+++ src/components/Appointment.js
@@ -8,10 +8,13 @@ class Appointment extends Component {
         <div className="media mt-3">
             <div className="media-body">
                 <h3 className="mt-0">{pet}</h3>
-                <p className="card-text">Owner Name: <span>{owner}</span></p>
-                <p className="card-text">Date: <span>{date}</span></p>
-                <p className="card-text">Time: <span>{time}</span></p>
-                <p className="card-text">Symptoms: <span>{symptoms}</span></p>
+                <p className="card-text"><span>Owner Name: </span>{owner}</p>
+                <p className="card-text"><span>Date: </span>{date}</p>
+                <p className="card-text"><span>Time: </span>{time}</p>
+                <p className="card-text"><span>Symptoms: </span><br />{symptoms}</p>
+                <button className="btn btn-danger">Erase &times;
+
+                </button>
             </div>
         </div>
     )
